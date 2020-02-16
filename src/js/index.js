ebb06eeed4c117c39afeb28b79584b3141830e39
Gastón Rivata <gastonrivata@gmail.com>
2020-02-16 18:22:46 -0300

    add sugerencias

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,65 @@
 const API_KEY = 'api_key=QuYi0qDFEcf3j0Dzr2HvdncBvJgU46HR';
-const endpointRandom =
-    'http://api.giphy.com/v1/gifs/random?' + 'api_key=' + API_KEY;
+const endpointRandom = 'http://api.giphy.com/v1/gifs/random?' + 'api_key=' + API_KEY;
 
 const cargarContenido = () => {
-    buscarSugerencias();
     cargarEventos();
-}
-
-const buscarSugerencias = () => {
-    let url = `https://api.giphy.com/v1/gifs/trending?${API_KEY}`;
-
-    fetchContent(url, null)
-        .then(mostrarSugerencias)
-        .catch((error) => {
-            console.error('Error', error);
-        });
+    buscarSugerencias();
+    buscarTendencias();
 };
 
 const cargarEventos = () => {
     const botonCrearGuifos = document.getElementById('boton-crear-gif');
+    const inputBuscar = document.getElementById('input-buscar');
 
     botonCrearGuifos.addEventListener('click', () => {
-        window.location.href = "/crear-gif.html"
+        window.location.href = '/crear-gif.html';
     });
-}
+    inputBuscar.addEventListener('keypress', presionarEnterInput);
+};
+
+const presionarEnterInput = event => {
+    if (event.keyCode === 13 && event.srcElement.value) {
+        console.log('Hay valor');
+    }
+};
+
+const buscarSugerencias = () => {
+    const url = `https://api.giphy.com/v1/gifs/random?${API_KEY}`;
+    let promesas = [];
+
+    for (let index = 0; index <= 3; index++) {
+        promesas.push(fetchContent(url));
+    }
+
+    Promise.all(promesas)
+        .then(mostrarSugerencias)
+        .catch(logError);
+};
+
+const mostrarSugerencias = sugerencias => {
+    let wrapper = document.getElementById('sugerencias-card-wrapper');
+    wrapper.innerHTML = '';
+
+    sugerencias.forEach(sugerencia => {
+        let gifContainer = document.createElement('div');
+        gifContainer.classList.add('sugerencias-card-wrapper--img-container');
+
+        let gif = document.createElement('img');
+        gif.src = sugerencia.data.images.fixed_width_downsampled.url;
+        gif.classList.add('sugerencias-card-wrapper--img-container--note');
+
+        gifContainer.appendChild(gif);
+        wrapper.appendChild(gifContainer);
+    });
+};
+
+const buscarTendencias = () => {
+    const url = `https://api.giphy.com/v1/gifs/trending?${API_KEY}&limit=6`;
+
+    fetchContent(url, null)
+        .then(mostrarTendencias)
+        .catch(logError);
+};
 
 const fetchContent = async (url, headers) => {
     let respuesta = await fetch(url, headers);
@@ -32,32 +68,23 @@ const fetchContent = async (url, headers) => {
     return datos;
 };
 
-const mostrarSugerencias = (sugerencias) => {
-    console.log(sugerencias);
-    let wrapper = document.getElementById('sugerencias-card-wrapper');
+const mostrarTendencias = tendencias => {
+    let wrapper = document.getElementById('tendencias-card-wrapper');
     wrapper.innerHTML = '';
 
-    // for (let i = 0; i < 3; i++) {
-    //     let gifContainer = document.createElement('div');
-    //     gifContainer.classList.add('sugerencias-card-wrapper--img-container');
-
-    //     let gif = document.createElement('img')
-    //     gif.src = sugerencias.data[i].images.original.url
-    //     gif.classList.add('sugerencias-card-wrapper--img-container--note');
-
-    //     gifContainer.appendChild(gif);
-    //     wrapper.appendChild(gifContainer);
-    // }
-
-    sugerencias.data.forEach((sugerencia) => {
+    tendencias.data.forEach(tendencia => {
         let gifContainer = document.createElement('div');
-        gifContainer.classList.add('sugerencias-card-wrapper--img-container');
+        gifContainer.classList.add('tendencias-card-wrapper--img-container');
 
         let gif = document.createElement('img');
-        gif.src = sugerencia.images.original.url;
-        gif.classList.add('sugerencias-card-wrapper--img-container--note');
+        gif.src = tendencia.images.fixed_width_downsampled.url;
+        gif.classList.add('tendencias-card-wrapper--img-container--note');
 
         gifContainer.appendChild(gif);
         wrapper.appendChild(gifContainer);
     });
+};
+
+const logError = (error, seccion) => {
+    console.error(`Ocurrió un error al obtener la información: `, error);
 };
